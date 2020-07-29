4d0271fd85757766a2a30ecd82520d85eef40c47
Gabriela <gabrielam.proyectos@gmail.com>
2020-07-29 12:52:32 -0500

    Corrección de bugs en emailJS.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// ------------- Animaciones -------------
+
 const images = document.querySelectorAll('img');
 images.forEach((image)=>{
     image.classList.add('fadeIn')
 })
+
+// ------------- Progress Skills -------------
 
 function Circlle(el){
     $(el).circleProgress({fill: {color: '#FFAB00'}})
@@ -13,26 +17,32 @@ function Circlle(el){
 };
 Circlle('.round');
 
-var email_value = document.getElementsByClassName('email')[0].value;
+// ------------- EmailJS -------------
 
 $('#contact').on('submit', function(event) {
-    event.preventDefault(); // prevent reload
-    var formData = new FormData(this);
-    
-    formData.append('service_id', 'gabrielam.proyectos');
-    formData.append('template_id', 'template_2wqM7Kr9');
-    formData.append('user_id', 'user_0mxxMl4fBIPxARsZGdniB');
-    formData.append('email', email_value);
+    let email_value = document.getElementsByClassName('email')[0].value;
 
- 
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
-        type: 'POST',
-        data: formData,
-        contentType: false, // auto-detection
-        processData: false // no need to parse formData to string
-    }).done(function() {
-        alert('Recibido, ¡muchas gracias!');
-    }).fail(function(error) {
-        alert('Oops... ' + JSON.stringify(error));
-    });
+    if (email_value){
+        event.preventDefault(); // prevent reload
+        let formData = new FormData(this);
+        
+        formData.append('service_id', 'gabrielam.proyectos');
+        formData.append('template_id', 'template_2wqM7Kr9');
+        formData.append('user_id', 'user_0mxxMl4fBIPxARsZGdniB');
+        formData.append('email', email_value);
+
+    
+        $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+            type: 'POST',
+            data: formData,
+            contentType: false, // auto-detection
+            processData: false // no need to parse formData to string
+        }).done(function() {
+            alert('Recibido, ¡muchas gracias!');
+        }).fail(function(error) {
+            alert('Oops... ' + JSON.stringify(error));
+        });
+    } else{
+        alert('Ingresa una dirección de correo.')
+    }
 });
